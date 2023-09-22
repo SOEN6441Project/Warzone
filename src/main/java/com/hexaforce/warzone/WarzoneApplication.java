3359cbaf3425eaecf1a42b597d87a300f74843fd
Deniz <denzz312312@gmail.com>
2023-09-22 11:54:19 -0400

    GH-2: [CHANGE] Modify CI flow

--- src/main/java/com/hexaforce/warzone/WarzoneApplication.java
+++ src/main/java/com/hexaforce/warzone/WarzoneApplication.java
@@ -3,6 +3,10 @@ package com.hexaforce.warzone;
 public class WarzoneApplication {
 
   public static void main(String[] args) {
-    System.out.println("Hey");
+    System.out.println("Hey"
+
+
+
+    );
   }
 }
