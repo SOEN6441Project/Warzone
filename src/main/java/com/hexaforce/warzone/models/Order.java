6dfa756f5c6d1b015b36c0ba6d02dabafe17cf18
Habeeb Dashti <habeeb.dashti09@gmail.com>
2023-10-03 14:31:20 -0400

    GH-14 : [ADD] Order class created

---
+++ src/main/java/com/hexaforce/warzone/models/Order.java
@@ -0,0 +1,11 @@
+package com.hexaforce.warzone.models;
+
+import com.hexaforce.warzone.controllers.PlayerController;
+import java.util.*;
+
+public class Order {
+  public static void createPlayers() {
+    PlayerController executeOrder = new PlayerController();
+    executeOrder.run();
+  }
+}
