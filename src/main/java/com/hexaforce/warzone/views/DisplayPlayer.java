9067cb78d071097c13e6340599939928277f3a31
Habeeb Dashti <habeeb.dashti09@gmail.com>
2023-10-04 11:26:58 -0400

    GH-14 : [ADD] Added Display viewer class

---
+++ src/main/java/com/hexaforce/warzone/views/DisplayPlayer.java
@@ -0,0 +1,18 @@
+package com.hexaforce.warzone.views;
+
+import java.util.List;
+
+/** Prints player names from the list of players */
+public class DisplayPlayer {
+  public static void showPlayers(List<String> p_playerList) {
+    for (String player : p_playerList) {
+      System.out.print(player.toUpperCase() + "\t");
+    }
+  }
+
+  public static void showAssignment(List<String> p_Assignment) {
+    for (String assigned : p_Assignment) {
+      System.out.println(assigned + "\t");
+    }
+  }
+}
