2d4f73722feaf5f3a7ca2bb6da6e4259db98ad4a
Habeeb Dashti <habeeb.dashti09@gmail.com>
2023-10-04 17:05:51 -0400

    GH-14 : [CHANGE] Minor design changes

--- src/main/java/com/hexaforce/warzone/controllers/PlayerController.java
+++ src/main/java/com/hexaforce/warzone/controllers/PlayerController.java
@@ -1,10 +1,10 @@
 package com.hexaforce.warzone.controllers;
 
+import com.hexaforce.warzone.models.Map;
 import com.hexaforce.warzone.models.Player;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import com.hexaforce.warzone.views.PlayerView;
+import java.io.Serializable;
+import java.util.*;
 
 /**
  * PlayerController contains the functionalities of the player model class
@@ -12,27 +12,38 @@ import java.util.Scanner;
  * @author Habeeb Dashti
  * @version 1.0
  */
-public class PlayerController {
+public class PlayerController implements Serializable {
 
   /** Stores a list of player created */
-  public static List<String> d_playerList = new ArrayList<>();
+  public static List<String> d_playerList = Player.d_playerNames;
   /** Stores random countries assigned to the players */
-  public static List<String> d_countryPlayerList = new ArrayList<>();
+  public static List<String> d_countryPlayerList = Player.d_countryToPlayer;
   /** Works as checker for entering, continuing and exiting the loop */
   boolean d_flag;
 
+  public Map d_mapModel;
+
+  public PlayerController(Map p_mapModel) {
+    d_mapModel = p_mapModel;
+  }
+
   /** This method creates players depending on the commands entered by the user */
-  public void playerCreation() {
+  public List<String> playerCreation() {
     Scanner l_scanner = new Scanner(System.in);
     d_flag = true;
     while (d_flag) {
-      System.out.print("\nEnter a command to create/remove players OR exit: ");
+      System.out.print("\nEnter a command : ");
       String l_playerCommand = l_scanner.nextLine();
       String[] l_tokens = l_playerCommand.split(" ");
+      if (l_playerCommand.equals("assigncountries")) {
+        randomCountryAssignment();
+        System.out.println("\nPlayers Saved and Countries assigned.");
+        d_flag = false;
+        break;
+      }
       if (l_playerCommand.startsWith("exit") && (d_playerList.size() >= 2)) {
         System.out.println("\nPlayers saved");
         this.d_flag = false;
-
         break;
       } else if (l_playerCommand.startsWith("exit") && (d_playerList.size() < 2)) {
         System.out.println("\nCreate at least 2 players to start the game!");
@@ -76,7 +87,7 @@ public class PlayerController {
             playerCreation();
           } else {
             System.out.println("\nPlayers Ready!");
-            showPlayers();
+            PlayerView.showPlayers(d_playerList);
             playerCreation();
           }
         }
@@ -93,6 +104,7 @@ public class PlayerController {
                                     3. gameplayer -add <playername> -remove <player's name>""");
       }
     }
+    return d_playerList;
   }
 
   /**
@@ -113,33 +125,28 @@ public class PlayerController {
     d_playerList.remove(p_playerName);
   }
 
-  /** Prints player names from the list of players */
-  public static void showPlayers() {
-
-    for (String player : d_playerList) {
-      System.out.print(player.toUpperCase() + " ");
-    }
-  }
-
   /**
    * The following method assigns countries randomly to the players
    *
-   * @param p_playerObject allows to call required methods and entities from Player class
+   * @return list of countries assigned to players randomly
    */
-  public static void randomCountryAssignment(Player p_playerObject) {
-    List<String> l_countries = p_playerObject.getCountryName();
+  public List<String> randomCountryAssignment() {
+    Collection<String> countries = this.d_mapModel.getCountries().keySet();
+    List<String> l_countries = new ArrayList<String>(countries);
     int l_numberOfPlayer = d_playerList.size();
     int l_numberOfCountries = l_countries.size();
     int l_countriesPerPlayer = l_numberOfCountries / l_numberOfPlayer;
     for (String player : d_playerList) {
       Random l_random = new Random();
-      d_countryPlayerList.add(d_playerList.toString().toUpperCase());
+      d_countryPlayerList.add(player.toUpperCase());
       for (int i = 0; i < l_countriesPerPlayer; i++) {
-        int l_randomToken = l_random.nextInt(l_numberOfCountries);
+        int l_randomToken = l_random.nextInt(l_countriesPerPlayer);
         String l_randomCountry = l_countries.get(l_randomToken);
         d_countryPlayerList.add(l_randomCountry);
         l_countries.remove(l_randomCountry);
       }
     }
+    PlayerView.showAssignment(d_countryPlayerList);
+    return d_countryPlayerList;
   }
 }
