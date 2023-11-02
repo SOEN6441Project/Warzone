967163ae83bc72c2c315316f4b94039a86b25460
Habeeb Dashti <habeeb.dashti09@gmail.com>
2023-11-02 18:40:35 -0400

    GH-37: [ADD] Added deploy order functionality to the player service

--- src/main/java/com/hexaforce/warzone/services/PlayerService.java
+++ src/main/java/com/hexaforce/warzone/services/PlayerService.java
@@ -1,5 +1,17 @@
 package com.hexaforce.warzone.services;
 
+import com.hexaforce.warzone.models.Player;
+
+/** This class represents player's services to be performed on requirement. */
 public class PlayerService {
-    
+
+  /**
+   * creates the order on the commands entered by the player.
+   *
+   * @param p_inputCommand command entered by the user
+   * @param p_player player to create deploy order
+   */
+  public void deployOrder(String p_inputCommand, Player p_player) {
+    // TODO: implement the 'deployOrder' functionality
+  }
 }
