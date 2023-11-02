cf923791c6529fab9d1d43e29370401297b7218e
Habeeb Dashti <habeeb.dashti09@gmail.com>
2023-11-02 19:02:34 -0400

    GH-37: [WIP] Player creation, deletion, assign countries & order deployment under construction

--- src/main/java/com/hexaforce/warzone/services/PlayerService.java
+++ src/main/java/com/hexaforce/warzone/services/PlayerService.java
@@ -2,7 +2,11 @@ package com.hexaforce.warzone.services;
 
 import com.hexaforce.warzone.models.Player;
 
-/** This class represents player's services to be performed on requirement. */
+/**
+ * This class represents player's services to be performed on requirement.
+ *
+ * @author Habeeb Dashti
+ */
 public class PlayerService {
 
   /**
@@ -13,5 +17,28 @@ public class PlayerService {
    */
   public void deployOrder(String p_inputCommand, Player p_player) {
     // TODO: implement the 'deployOrder' functionality
+  }
+  /**
+   * Adds player's name(s) to the list
+   *
+   * @param p_playerName passes the name(s) to be added to the list
+   */
+  public void addPlayer(String p_playerName) {
+    // TODO: implement the 'addPlayer' functionality
+    //    d_playerList.add(p_playerName);
+  }
+
+  /**
+   * Removes player's name(s) from the list
+   *
+   * @param p_playerName passes the name(s) to be removed from the list
+   */
+  public void removePlayer(String p_playerName) {
+    // TODO: implement the 'removePlayer' functionality
+    //    d_playerList.remove(p_playerName);
+  }
+
+  public void randomCountryAssignment() {
+    // TODO: implement the 'assign random country' functionality
   }
 }
