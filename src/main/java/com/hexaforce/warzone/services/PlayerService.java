d23683b8e169441b53f516c5fb7aede24f8c9bcd
Habeeb Dashti <habeeb.dashti09@gmail.com>
2023-11-03 13:05:21 -0400

    GH-37: [WIP] Completed deploy order and added validate army functionality

--- src/main/java/com/hexaforce/warzone/services/PlayerService.java
+++ src/main/java/com/hexaforce/warzone/services/PlayerService.java
@@ -1,6 +1,10 @@
 package com.hexaforce.warzone.services;
 
+import com.hexaforce.warzone.models.Order;
 import com.hexaforce.warzone.models.Player;
+import com.hexaforce.warzone.utils.CommonUtil;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This class represents player's services to be performed on requirement.
@@ -16,8 +20,46 @@ public class PlayerService {
    * @param p_player player to create deploy order
    */
   public void deployOrder(String p_inputCommand, Player p_player) {
-    // TODO: implement the 'deployOrder' functionality
+    String l_country = p_inputCommand.split(" ")[1];
+    String l_armies = p_inputCommand.split(" ")[2];
+    List<Order> l_orderList;
+    Order l_orderObject;
+    if (CommonUtil.isCollectionEmpty(p_player.getD_ordersToExecute())) {
+      l_orderList = new ArrayList<>();
+    } else {
+      l_orderList = p_player.getD_ordersToExecute();
+    }
+    if (validateDeployArmy(p_player, l_armies)) {
+      System.out.println("\nError! You do not have enough army to deploy.");
+    } else {
+      Integer l_remainingArmies =
+          p_player.getD_noOfUnallocatedArmies() - Integer.parseInt(l_armies);
+      l_orderObject =
+          new Order(p_inputCommand.split(" ")[0], l_country, Integer.parseInt(l_armies));
+      l_orderList.add(l_orderObject);
+      p_player.setD_ordersToExecute(l_orderList);
+      p_player.setD_noOfUnallocatedArmies(l_remainingArmies);
+      System.out.println("\nAdded your order to execute successfully in the queue.");
+    }
   }
+
+  /**
+   * This function validates if the player has enough remaining armies before deployment
+   *
+   * @param p_player contains the respective player who passed the order
+   * @param l_armies contains the number of armies passed by the player
+   * @return l_flag 'true' for not having enough army and 'false' for having enough
+   */
+  private boolean validateDeployArmy(Player p_player, String l_armies) {
+    boolean l_flag;
+    if (p_player.getD_noOfUnallocatedArmies() >= Integer.parseInt(l_armies)) {
+      l_flag = false;
+    } else {
+      l_flag = true;
+    }
+    return l_flag;
+  }
+
   /**
    * Adds player's name(s) to the list
    *
